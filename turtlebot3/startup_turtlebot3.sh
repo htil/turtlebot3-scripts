d56705fc7c3f50cd52251da635ef4bb8604f3b4f
Chris Crawford <chris35469@gmail.com>
2025-10-26 16:06:25 -0500

    Add service file update startup

--- turtlebot3/startup_turtlebot3.sh
+++ turtlebot3/startup_turtlebot3.sh
@@ -2,13 +2,14 @@
 
 # Turtlebot SBC
 source /opt/ros/humble/setup.bash
-source ~/turtlebot3_ws/install/setup.bash
+source /home/csc/turtlebot3_ws/install/setup.bash
 
 # Setup ROS_DOMAIN_ID
-export ROS_DOMAIN_ID=30
- 
+export ROS_DOMAIN_ID=31
+export LDS_MODEL=LDS-01
+
 # ROS Middleware
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp  
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
 # Start bringup program [Turtlebot SBC]
 export TURTLEBOT3_MODEL=burger
