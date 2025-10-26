c21e4a0034094b803143e9b95615e87cabc914f3
Chris Crawford <chris35469@gmail.com>
2025-10-26 18:00:26 -0500

    Fix errors in install_packages script

--- turtlebot3/install_packages.sh
+++ turtlebot3/install_packages.sh
@@ -15,12 +15,14 @@ rm -r turtlebot3_cartographer turtlebot3_navigation2
 cd ~/turtlebot3_ws/
 echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
 colcon build --symlink-install --parallel-workers 1
-echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
-source ~/.bashrc
+echo 'source /home/csc/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
+source /home/csc/.bashrc
+source /home/csc/turtlebot3_ws/install/setup.bash
 sudo cp `ros2 pkg prefix turtlebot3_bringup`/share/turtlebot3_bringup/script/99-turtlebot3-cdc.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
-source ~/.bashrc
+source /home/csc/.bashrc
+export LDS_MODEL=LDS-01
 echo "Remember to setup ROS_DOMAIN_ID"
 echo "See https://emanual.robotis.com/docs/en/platform/turtlebot3/sbc_setup/#sbc-setup"
