35cc40e9b733a5450a9244a3fba6860309928635
Chris Crawford <chris35469@gmail.com>
2025-10-26 20:08:29 -0500

    Add setup_bash scripts

--- turtlebot3/install_packages.sh
+++ turtlebot3/install_packages.sh
@@ -13,16 +13,16 @@ git clone -b humble https://github.com/ROBOTIS-GIT/coin_d4_driver
 cd ~/turtlebot3_ws/src/turtlebot3
 rm -r turtlebot3_cartographer turtlebot3_navigation2
 cd ~/turtlebot3_ws/
-echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+#echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
 colcon build --symlink-install --parallel-workers 1
-echo 'source /home/csc/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
-source /home/csc/.bashrc
-source /home/csc/turtlebot3_ws/install/setup.bash
+#echo 'source /home/csc/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
+#source /home/csc/.bashrc
+#source /home/csc/turtlebot3_ws/install/setup.bash
 sudo cp `ros2 pkg prefix turtlebot3_bringup`/share/turtlebot3_bringup/script/99-turtlebot3-cdc.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo udevadm trigger
-echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
-source /home/csc/.bashrc
-export LDS_MODEL=LDS-01
+#echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
+#source /home/csc/.bashrc
+#export LDS_MODEL=LDS-01
 echo "Remember to setup ROS_DOMAIN_ID"
 echo "See https://emanual.robotis.com/docs/en/platform/turtlebot3/sbc_setup/#sbc-setup"
---
+++ turtlebot3/setup_bash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo 'source /home/csc/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
+echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
