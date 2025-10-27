681d6577d44b8e0efb4b5c4d3e3ffcbbe88b2ffe
Chris Crawford <chris35469@gmail.com>
2025-10-26 20:19:19 -0500

    Update install_packaged

--- turtlebot3/setup_bash.sh
+++ turtlebot3/setup_bash.sh
@@ -3,3 +3,6 @@
 echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
 echo 'source /home/csc/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
 echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
+source /opt/ros/humble/setup.bash
+source /home/csc/turtlebot3_ws/install/setup.bash
+ros2
